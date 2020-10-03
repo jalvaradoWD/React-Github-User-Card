805dc72bcf590896a4cbf4c2a86d44d958bdf10b
jalvaradoWD <josealvarado1337@gmail.com>
2020-10-03 13:32:45 -0500

    Added a component lifecycle to the App component.
    Added axios to the project for API calls.

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <>
-      <h1>Hello world</h1>
-    </>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({
+      users: [...res.data],
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <h1>GitHub User Cards</h1>
+        <ul>
+          {this.state.users.map((user) => (
+            <li>{user.login}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;
